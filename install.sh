ef9a222b1f535b432e554befd302b16f83652fa8
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-03-25 16:48:41 +0800

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 ﻿#!/bin/bash
-#FFMPEG安装脚本
+#FFMPEG installation script
 
-#  版权所有（C）2007-2016 Sherin.co.in。
+#  Copyright (C) 2007-2014 Sherin.co.in. All rights reserved.
 #
-#  此程序是免费软件; 您可以重新分发它和/或修改
-#  根据发布的GNU通用公共许可证的条款
-#  自由软件基金会; 版本2的许可证，或
-#  （可选）任何更新的版本。
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  这个程序是分发的，希望它会有用，
-#  但没有任何保证; 甚至没有隐含的保证
-#  适销性或特定用途的适用性。 看到
-#  GNU通用公共许可证了解更多详情。
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#  您应该已经收到GNU通用公共许可证的副本
-#  以及这个程序; 如果没有，写入自由软件
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 RED='\033[01;31m'
 RESET='\033[0m'
@@ -41,6 +41,12 @@ echo ""
 echo -e "雇用服务器管理员$ GREEN @ 50％$ RESET的折扣率. "
 echo -e "雇用cpanel / directadmin支持计划 $GREEN @ 50% $RESET 折扣."
 echo -e "雇用一个半导体团队来管理您的服务器$GREEN  @ 50% $RESET 折扣."
+echo -e "设置专门的支持团队$GREEN  @ 50%  $RESET 折扣."
+echo ""
+echo -e "登录联系 \033[4mjie5343@126.com$RESET for  得到上述折扣券，或"
+echo -e "jie5343我们 @ \033[1m \033[4m SyslintSkype$RESET   用于以此折扣下订单."
+echo ""
+echo "此优惠只适合您 !!!"
 echo ""
 echo -e "$RED"
 echo "**************************************************************************"
--- mediainfo_64.sh
+++ mediainfo_64.sh
@@ -1,25 +1,26 @@
-﻿#!/bin/bash
-#FFMPEG安装脚本
+#!/bin/bash
+#FFMPEG installation script
 
-#  版权所有（C）2007-2016 Sherin.co.in。。
+#  Copyright (C) 2007-2014 Sherin.co.in. All rights reserved.
 #
-#  此程序是免费软件; 您可以重新分发它和/或修改
-#  根据发布的GNU通用公共许可证的条款
-#  自由软件基金会; 版本2的许可证，或
-#  （可选）任何更新的版本。
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  这个程序是分发的，希望它会有用，
-#  但没有任何保证; 甚至没有隐含的保证
-#  适销性或特定用途的适用性。 看到
-#  GNU通用公共许可证了解更多详情。
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
-#  您应该已经收到GNU通用公共许可证的副本
-#  以及这个程序; 如果没有，写入自由软件
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
 _url=`cat ./url.txt`
+SOURCE_URL=$_url/mediainfo_64
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
@@ -32,14 +33,9 @@ echo "Removing old source"
 rm -vrf mediainfo_64*
 #wget http://downloads.sourceforge.net/zenlib/libzen0-0.4.29-1.x86_64.CentOS_6.rpm
 #wget http://sourceforge.net/projects/mediainfo/files/binary/libmediainfo0/0.7.64/libmediainfo0-0.7.64-1.x86_64.CentOS_6.rpm
-#http://sourceforge.net/projects/mediainfo/files/binary/mediainfo/0.7.64/mediainfo-0.7.64-1.x86_64.CentOS_6.rpm
-if [ -f "$_package" ]
-	then
-		echo "$_package found, Skip Downloads"
-else
-		echo "$_package not found, Try Downloading......"
-		wget $_url/$_package
-fi
+http://sourceforge.net/projects/mediainfo/files/binary/mediainfo/0.7.64/mediainfo-0.7.64-1.x86_64.CentOS_6.rpm
+wget $SOURCE_URL/$_package
+#mv /root/ffmpeginstaller/source/mediainfo_64/$_package $INSTALL_SDIR
 yum -y install unzip
 unzip mediainfo_64.zip
 cd mediainfo_64
