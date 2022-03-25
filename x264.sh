69a70a4166adf37fae3534751015eb3415e73ff0
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-03-25 16:52:05 +0800

    Add files via upload

--- x264.sh
+++ x264.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
-#FFMPEG installation script
+#FFMPEG安装脚本
 
-#  Copyright (C) 2007-2014 Sherin.co.in. All rights reserved.
+#  版权所有（C）2007-2016 Sherin.co.in。 
 #
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
+#  此程序是免费软件; 您可以重新分发它和/或修改
+#  根据发布的GNU通用公共许可证的条款
+#  自由软件基金会; 版本2的许可证，或
+#  （可选）任何更新的版本。
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#  这个程序是分发的，希望它会有用，
+#  但没有任何保证; 甚至没有隐含的保证
+#  适销性或特定用途的适用性。 看到
+#  GNU通用公共许可证了解更多详情。
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
+#  您应该已经收到GNU通用公共许可证的副本
+#  以及这个程序; 如果没有，写入自由软件
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 RED='\033[01;31m'
 RESET='\033[0m'
 INSTALL_SDIR='/usr/src/ffmpegscript'
 _url=`cat ./url.txt`
-SOURCE_URL=$_url/x264
 INSTALL_DDIR='/usr/local/cpffmpeg'
 export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
 export TMPDIR=$HOME/tmp
-_package='x264'
+_package='last_stable_x264.tar.bz2'
 clear
 sleep 2
 echo -e $RED"Installation of $_package ....... started"$RESET
 ldconfig
 cd $INSTALL_SDIR
 echo "Removing old source"
-   rm -vrf $INSTALL_SDIR/x264-snapshot*
-	git clone git://git.videolan.org/x264.git
-	cd x264/
+yum -y install bzip2
+rm -vrf $INSTALL_SDIR/x264-snapshot*
+   
+if [ -f "$_package" ]
+	then
+		echo "$_package found, Skip Downloads"
+else
+		echo "$_package not found, Try Downloading......"
+		wget https://download.videolan.org/x264/snapshots/$_package
+fi  
+tar xvjf last_stable_x264.tar.bz2
+cd x264-snapshot-*-stable/
 	./configure  --prefix=$INSTALL_DDIR --enable-shared --disable-asm
 	make -j$cpu
 	make install
