ac9f4dcee3d90113a65826105dd177d282e1e47e
xu5343 <32597757+xu5343@users.noreply.github.com>
2022-03-25 13:18:26 +0800

    yasm-1.2.0
    yasm-1.2.0

---
+++ yasm.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#FFMPEG安装脚本
+
+#  版权所有（C）2007-2022 Sherin.co.in。
+#
+#  此程序是免费软件; 您可以重新分发它和/或修改
+#  根据发布的GNU通用公共许可证的条款
+#  自由软件基金会; 版本2的许可证，或
+#  （可选）任何更新的版本。
+#
+#  这个程序是分发的，希望它会有用，
+#  但没有任何保证; 甚至没有隐含的保证
+#  适销性或特定用途的适用性。 看到
+#  GNU通用公共许可证了解更多详情。
+#
+#  您应该已经收到GNU通用公共许可证的副本
+#  以及这个程序; 如果没有，写入自由软件
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+RED='\033[01;31m'
+RESET='\033[0m'
+INSTALL_SDIR='/usr/src/ffmpegscript'
+_url=`cat ./url.txt`
+SOURCE_URL=$_url/yasm
+INSTALL_DDIR='/usr/local/cpffmpeg'
+export cpu=`cat "/proc/cpuinfo" | grep "processor"|wc -l`
+export TMPDIR=$HOME/tmp
+_package='yasm-1.2.0.tar.gz'
+sleep 2
+echo -e $RED"Installation of $_package ....... started"$RESET
+ldconfig
+cd $INSTALL_SDIR
+echo "Removing old source"
+rm -vrf yasm*
+if [ -f "$_package" ]
+	then
+		echo "$_package found, Skip Downloads"
+else
+		echo "$_package not found, Try Downloading......"
+		wget https://www.tortall.net/projects/yasm/releases/$_package
+fi
+tar -xvzf $_package
+cd  yasm-1.2.0/
+	./configure --prefix=/usr/local/cpffmpeg/ 
+make -j$cpu
+make install
+ln -sf /usr/local/cpffmpeg/bin/yasm /usr/local/bin/yasm
+ldconfig
+echo -e $RED"Installation of $_package ....... Completed"$RESET
+sleep 2
